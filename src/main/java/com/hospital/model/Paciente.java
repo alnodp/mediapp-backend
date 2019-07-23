31b0a3cefb4d06765c3097ecbdc3948c3f385cef
AndresL <amlopezr90@gmail.com>
2019-07-22 19:23:28 -0500

    implementando swagger2

--- src/main/java/com/hospital/model/Paciente.java
+++ src/main/java/com/hospital/model/Paciente.java
@@ -1,33 +1,42 @@
 package com.hospital.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.validator.constraints.Email;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 
+@ApiModel(description = "Información del Paciente")
 @Entity
+@Table(name = "paciente")
 public class Paciente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idPaciente;
 
+    @ApiModelProperty(notes = "Los nombres deben tener mínimo 3 caracteres")
     @Size(min = 3, message = "Los nombres deben tener mínimo 3 caracteres")
     @Column(name = "nombres", nullable = false, length = 70)
     private String nombres;
 
+    @ApiModelProperty(notes = "Los apellidos deben tener mínimo 3 caracteres")
     @Size(min = 3, message = "Los apellidos deben tener mínimo 3 caracteres")
     @Column(name = "apellidos", nullable = false, length = 70)
     private String apellidos;
 
+    @ApiModelProperty(notes = "El dni debe tener 8 caracteres")
     @Size(min = 8, max = 8, message = "El dni debe tener 8 caracteres")
     @Column(name = "dni", nullable = false, length = 8)
     private String dni;
 
+    @ApiModelProperty(notes = "La dirección debe tener mínimo 3 caracteres")
     @Size(min = 3, max = 150, message = "La dirección debe tener mínimo 3 caracteres")
     @Column(name = "direccion", nullable = true, length = 150)
     private String direccion;
 
+    @ApiModelProperty(notes = "El teléfono debe tener 9 caracteres")
     @Size(min = 9, max = 9, message = "El teléfono debe tener 9 caracteres")
     @Column(name = "telefono", nullable = false, length = 9)
     private String telefono;
